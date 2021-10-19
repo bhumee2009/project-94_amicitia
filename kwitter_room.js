6d82fe922a86e84acb5ce97f513c34d54807601e
bhumee2009 <84014837+bhumee2009@users.noreply.github.com>
2021-10-19 17:35:16 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyA8FWx3lGQqY-rEWamWcSBtThIdE7TO_Gk",
+    authDomain: "amicitia-1a74e.firebaseapp.com",
+    databaseURL: "https://amicitia-1a74e-default-rtdb.firebaseio.com",
+    projectId: "amicitia-1a74e",
+    storageBucket: "amicitia-1a74e.appspot.com",
+    messagingSenderId: "961579965102",
+    appId: "1:961579965102:web:8e501c25ae5744e064eb3f"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
